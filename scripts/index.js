8c6a0991352fd508dba05452f6f5cc0dd3adc933
Chris W <cmwestrick@gmail.com>
2022-12-04 10:29:55 -0600

    12.4.22.B

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,30 @@
-let initialCards [
-
-  {
+  const cardYosemite = {
     name: "Yosemite Valley"
     link: "../images/yosemite-valley.jpg"
     }
   
-  {
+  const cardLakeLouise = {
     name:"Lake Louise"
     link: "../images/lake-louise.png"
     }
-  {
+  const cardBaldMountains = {
     name:"Bald Mountains"
     link: "../images/bald-mountains.png"
     }  
       
-  {
+  const cardLatemar = {
     name:"Latemar"
     link: "../images/latemar.png"
     }  
     
-  {
+  const cardVanoise = {
     name:"Vanoise National Park"
     link: "../images/vanoise.png"
     }  
     
-  {
+  const cardLago = {
     name:"Lago di Braies"
     link: "../images/lago-di-braies.png"
     }
-    ]
+
+  let initialCards = { cardYosemite, cardLakeLouise, cardBaldMountains, cardLatemar, cardVanoise, cardLago }
