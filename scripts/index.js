640753ee67fec8d7888a86f58d597b9de64d9b0e
Chris W <cmwestrick@gmail.com>
2022-12-15 19:36:06 -0600

    12.15.22.B

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,72 @@
-const cardYosemite = {
-  name: "Yosemite Valley",
-  link: "../images/yosemite-valley.jpg",
-};
+// const cardYosemite = {
+//   name: "Yosemite Valley",
+//   link: "../images/yosemite-valley.jpg",
+// };
 
-const cardLakeLouise = {
-  name: "Lake Louise",
-  link: "../images/lake-louise.png",
-};
+// const cardLakeLouise = {
+//   name: "Lake Louise",
+//   link: "../images/lake-louise.png",
+// };
 
-const cardBaldMountains = {
-  name: "Bald Mountains",
-  link: "../images/bald-mountains.png",
-};
+// const cardBaldMountains = {
+//   name: "Bald Mountains",
+//   link: "../images/bald-mountains.png",
+// };
 
-const cardLatemar = {
-  name: "Latemar",
-  link: "../images/latemar.png",
-};
+// const cardLatemar = {
+//   name: "Latemar",
+//   link: "../images/latemar.png",
+// };
 
-const cardVanoise = {
-  name: "Vanoise National Park",
-  link: "../images/vanoise.png",
-};
+// const cardVanoise = {
+//   name: "Vanoise National Park",
+//   link: "../images/vanoise.png",
+// };
 
-const cardLago = {
-  name: "Lago di Braies",
-  link: "../images/lago-di-braies.png",
-};
+// const cardLago = {
+//   name: "Lago di Braies",
+//   link: "../images/lago-di-braies.png",
+// };
 
-let initialCards = {
-  cardYosemite,
-  cardLakeLouise,
-  cardBaldMountains,
-  cardLatemar,
-  cardVanoise,
-  cardLago,
-};
+// let initialCards = {
+//   cardYosemite,
+//   cardLakeLouise,
+//   cardBaldMountains,
+//   cardLatemar,
+//   cardVanoise,
+//   cardLago,
+// };
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "../images/yosemite-valley.jpg",
+  },
+
+  {
+    name: "Lake Louise",
+    link: "../images/lake-louise.png",
+  },
+
+  {
+    name: "Bald Mountains",
+    link: "../images/bald-mountains.png",
+  },
+
+  {
+    name: "Latemar",
+    link: "../images/latemar.png",
+  },
+
+  {
+    name: "Vanoise National Park",
+    link: "../images/vanoise.png",
+  },
+
+  {
+    name: "Lago di Braies",
+    link: "../images/lago-di-braies.png",
+  },
+];
 
 const openModal = null;
